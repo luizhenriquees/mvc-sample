5f73d56c902e0fa07775110b0d7f91eeb7729aa1
Luiz Henrique <luizhenrique321@gmail.com>
2016-04-08 18:06:16 -0300

    #8 -Criação da classe ClassificacaoController responsável por fazer o link entre a interface gráfica e a regra de negócio nas operações relacionadas a classificação.

---
+++ src/main/groovy/br/com/zg/trainning/controller/ClassificacaoController.groovy
@@ -0,0 +1,23 @@
+package br.com.zg.trainning.controller
+
+import br.com.zg.trainning.model.business.interfaces.impl.Classificacao2015
+import br.com.zg.trainning.model.entities.Campeonato
+import br.com.zg.trainning.model.entities.Time
+
+/**
+ * Created by luizhenrique on 08/04/16.
+ */
+class ClassificacaoController {
+
+	Classificacao2015 classificacao2015 = new Classificacao2015()
+
+	Time obterTimeLanterna(Campeonato campeonato){
+		Map<Integer, Time> timesOrdenados = classificacao2015.aplicaCriteriosDesempate(campeonato.timesParticipantes)
+		return classificacao2015.obterTimeLanterna(timesOrdenados)
+	}
+
+	Time obterTimeCampeao(Campeonato campeonato){
+		Map<Integer, Time> timesOrdenados = classificacao2015.aplicaCriteriosDesempate(campeonato.timesParticipantes)
+		return classificacao2015.obterTimeCampeao(timesOrdenados)
+	}
+}
