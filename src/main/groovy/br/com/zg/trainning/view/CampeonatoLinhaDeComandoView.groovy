e9b4645060810cddde045c881974c4de57ca10f7
Luiz Henrique <luizhenrique321@gmail.com>
2016-04-12 11:56:38 -0300

    #9 - Inclusão da operação de exibir tabela na classe CampeonatoLinhaDeComandoView

--- src/main/groovy/br/com/zg/trainning/view/CampeonatoLinhaDeComandoView.groovy
+++ src/main/groovy/br/com/zg/trainning/view/CampeonatoLinhaDeComandoView.groovy
@@ -4,30 +4,32 @@ import br.com.zg.trainning.model.entities.Time
 
 class CampeonatoLinhaDeComandoView extends EntradaDadosView implements CampeonatoView {
 
-    String pergunteNomeCampeonato() {
-        pergunte 'Qual o nome do Campeonato?'
-    }
+	String pergunteNomeCampeonato() {
+		pergunte 'Qual o nome do Campeonato?'
+	}
 
-    String pergunteOperacao() {
-        println ''
-        println 'Selecione uma operação'
-        println '[1] Saber o campeão'
-        println '[2] Saber o lanterna'
-        println '[3] Exibir tabela'
-        println '[0] Encerrar'
+	String pergunteOperacao() {
+		println ''
+		println 'Selecione uma operação'
+		println '[1] Saber o campeão'
+		println '[2] Saber o lanterna'
+		println '[3] Exibir tabela'
+		println '[0] Encerrar'
 
-        pergunte ''
-    }
+		pergunte ''
+	}
 
-    void exibaCampeao(Time time) {
-        println "O campeão é o $time.nome!"
-    }
+	void exibaCampeao(Time time) {
+		println "O campeão é o $time.nome!"
+	}
 
-    void exibaLanterna(Time time) {
-        println "O lanterna é o $time.nome."
-    }
+	void exibaLanterna(Time time) {
+		println "O lanterna é o $time.nome."
+	}
 
-    void exibaTabela(List<Time> times) {
-
-    }
+	void exibaTabela(List<Time> times) {
+		times.eachWithIndex { Time entry, int i ->
+            println("${i+1} ${entry.nome}")
+		}
+	}
 }
